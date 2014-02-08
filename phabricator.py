37818271bf3b83b454ad365ef149f3eb271cd4e8
Todd Wolfson <todd@twolfson.com>
2014-02-07 19:55:33 -0800

    Skipping line count and jumping to arc invocation

--- phabricator.py
+++ phabricator.py
@@ -1,8 +1,24 @@
+import path
+import sublime
 import sublime_plugin
+import subprocess
 
 
 class PhabricatorOpenCommand(sublime_plugin.WindowCommand):
     def run(self):
-        """Be awesome to each other."""
-        print 'hai'
-        pass
+        """Open a file inside of Phabricator with the selected lines."""
+        # Get the first selection
+        view = sublime.active_window().active_view()
+        first_sel = view.sel()[0]
+
+        # TODO: Find the lines that are selected
+        # begin_line = 2
+        lines = '2'
+
+        print first_sel, lines
+
+        # Find the filename
+        filepath = view.file_name()
+        dirname = path.os.dirname(filepath)
+        filename = path.os.basename(filepath)
+        # subprocess.Popen(['arc', 'browse',
