e91ff15c7a6c91cdb19598dc6ff69fb37b7b5b95
Todd Wolfson <todd@twolfson.com>
2014-02-07 20:28:44 -0800

    Cleaning up code

--- phabricator.py
+++ phabricator.py
@@ -19,12 +19,20 @@ class PhabricatorOpenCommand(sublime_plugin.WindowCommand):
 
         print first_sel, lines
 
-        # Find the filename
+        # Find the file directory and name
         filepath = view.file_name()
         filedir = os.path.dirname(filepath)
         filename = os.path.basename(filepath)
+
+        # Run `arc browse` and dump the output to the console
         child = subprocess.Popen(
             ['arc', 'browse', filename], cwd=filedir,
             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        # print child.stdout.read()
-        # print child.stderr.read()
+        stdout = child.stdout.read()
+        stderr = child.stderr.read()
+        if stdout or stderr:
+            print('Ran `arc browse {0}`'.format(filename))
+            if stdout:
+                print('STDOUT: {0}'.format(stdout))
+            if stderr:
+                print('STDERR: {0}'.format(stderr))
